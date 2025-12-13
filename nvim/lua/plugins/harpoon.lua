b977fd2874d0c874c92221c300122baaef49b797
André Marques <andremarques94@gmail.com>
2025-12-13 13:15:45 +0000

    took conflicting remaps out of harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,8 +6,5 @@ return {
 
         vim.keymap.set("n", "<leader>a", mark.add_file)
         vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
-
-        vim.keymap.set("n", "<C-u>", function() ui.nav_prev() end)
-        vim.keymap.set("n", "<C-i>", function() ui.nav_next() end)
     end
 }
