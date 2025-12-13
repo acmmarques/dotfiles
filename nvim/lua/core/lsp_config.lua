1f556f528ca16ab23ab2b4504c2faf9195f98b43
André Marques <115092696+acmmarques@users.noreply.github.com>
2025-12-13 15:41:59 +0000

    added bicep csharp to lspconfig

--- nvim/lua/core/lsp_config.lua
+++ nvim/lua/core/lsp_config.lua
@@ -4,6 +4,7 @@ vim.lsp.enable('ruby_lsp')
 vim.lsp.enable('herb_ls')
 vim.lsp.enable('html')
 vim.lsp.enable('tailwindcss')
+vim.lsp.enable('bicep-lsp')
 
 -- For lsp purposes, I'll turn every eruby filetype into
 -- a eruby.html filetype -> so that lsp works for html too
