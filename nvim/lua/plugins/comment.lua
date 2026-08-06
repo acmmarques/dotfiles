754af382c03280d8307045a5a55556c2a13f1ef3
André Marques <andre1.marques@vwgds.pt>
2026-08-06 16:49:37 +0100

    fixed comment.nvim bug

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,7 +1,19 @@
 return {
     'numToStr/Comment.nvim',
     opts = {
-        -- add any options here
-    }
-}
+        pre_hook = function(ctx)
+            local comment_ft = require('Comment.ft')
+            local ok, commentstring = pcall(comment_ft.calculate, ctx)
 
+            if ok and commentstring then
+                return commentstring
+            end
+
+            return comment_ft.get(vim.bo.filetype, ctx.ctype) or vim.bo.commentstring
+        end,
+    },
+    config = function(_, opts)
+        require('Comment.ft').set('tsx', { '//%s', '/*%s*/' })
+        require('Comment').setup(opts)
+    end,
+}
